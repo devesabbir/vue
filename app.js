3703898930c4eee230e41bc9bf3c9f9c17dba10c
devesabbir <heartedsabbirdhrubo@gmail.com>
2023-10-26 03:23:33 +0600

    Complete Todo App

--- app.js
+++ app.js
@@ -1,9 +1,47 @@
 const app = Vue.createApp({
+
     data () {
         return {
-            message: 'Hello world!',
-            foods: ['Apple', 'Orange', 'Pineapple']
+            item: '',
+            foods:JSON.parse(localStorage.getItem('foods')) || [],
+            editMode: false,
+            editIndex:-1,
+            editItem: ''
+          
         }
+    },
+
+    methods: {
+
+       addItemToArray: function () {
+        if (this.item) {
+          const arry = this.foods
+          arry.push(this.item)
+          localStorage.setItem('foods', JSON.stringify(arry))
+          this.item = ''
+        }
+          
+       },
+
+       updataArray: function () {
+           this.foods[this.editIndex] =  this.editItem
+           localStorage.setItem('foods', JSON.stringify(this.foods))
+           this.editItem= ''
+           this.editIndex = -1
+           this.editMode = false
+       },
+
+       removeFromArray: function (index) {
+          this.foods.splice(index, 1)
+          localStorage.setItem('foods', JSON.stringify(this.foods))
+       },
+
+       editItemFu: function (index) {
+           this.editItem = this.foods[index]
+           this.editMode = true
+           this.editIndex = index
+       },
+
     }
 })
 
